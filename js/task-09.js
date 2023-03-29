b9349a6a46c4f44a39d049416adb229eb3ef3871
number-nine <sergii.pokhylko@gmail.com>
2023-03-30 01:15:55 +0300

    tasks 7-9 + part of 10th

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,18 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const refs = {
+  outputSpan: document.querySelector('.color'),
+  randomizeBtn: document.querySelector('.change-color'),
+  body: document.body,
+};
+
+refs.randomizeBtn.addEventListener('click', onBtnClick);
+
+function onBtnClick(event) {
+  event.preventDefault();
+  const rndColor = getRandomHexColor();
+  refs.body.style.backgroundColor = rndColor;
+  refs.outputSpan.textContent = rndColor;
+}
--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,32 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const refs = {
+  controlsDiv: document.querySelector('#controls'),
+  createBtn: document.querySelector('[data-create]'),
+  boxesArea: document.querySelector('#boxes'),
+}
+
+refs.createBtn.addEventListener('click', onCreateClick);
+
+function onCreateClick(event){
+  event.preventDefault();
+  let htmlString = '';
+  console.dir(refs.controlsDiv.firstElementChild.value);
+  for (let i = 0; i < refs.controlsDiv.firstElementChild.value; i += 1) {
+    console.log(30+i*10);
+    const size = 30+i*10;
+  htmlString += `<div style="
+  border: 1px solid #333333;
+  width:${size}px;
+  height:${size}px;
+  "></div>`
+    
+  }
+  refs.boxesArea.innerHTML = htmlString;
+}
+
+// console.dir(refs.controlsDiv.firstChild.value);
+
+function createBoxes(amount) {}
